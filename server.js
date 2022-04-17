75c03f62c4de5480eac571a437ac3167aa5a5087
Sean Bannon <seannbannon@gmail.com>
2022-04-16 22:01:56 -0500

    hooked up mongoose db and added get and post methods

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require ('mongoose');
 const articleRouter = require('./routes/articles');
 const app = express();
 
+mongoose.connect('mongodb://localhost/blog')
+
 app.set ('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     const articles = [{
@@ -20,5 +23,8 @@ app.get('/', (req, res) => {
 
     res.render(`articles/index`, { articles: articles})
 })
+
+app.use('/articles', articleRouter)
+
 
 app.listen(3000);
