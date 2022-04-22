fac58f2b7b3f6b1d8f581ab9cd70b24eec2165f8
Sean Bannon <seannbannon@gmail.com>
2022-04-22 14:23:02 -0500

    adding show route to display neew articles

--- routes/articles.js
+++ routes/articles.js
@@ -3,15 +3,17 @@ const Article = require ('./../models/article');
 const router = express.Router();
 
 router.get('/new', (req, res) => {
-    res.render('articles/new')
+    res.render('articles/new', {article: new Article() })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    if (article === null) res.redirect('/')
+    res.render('articles/show', {article: article})
 })
 
 router.post('/', async (req, res) =>{
-    const article = new Article({
+    let article = new Article({
         title: req.body.title,
         description: req.body.description,
         markdown: req.body.markdown,
@@ -20,7 +22,7 @@ router.post('/', async (req, res) =>{
     article = await article.save()
     res.redirect(`/articles/${article.id}`)
     } catch (e) {
-        res.render('articles/new', { article: article })
+        res.render('articles/new', { article: article  })
     }
 });
 
