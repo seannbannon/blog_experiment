f292879d14ec05248d813616ca1cfcae259198ca
Sean Bannon <seannbannon@gmail.com>
2022-04-24 17:24:42 -0500

    delete button is working

--- server.js
+++ server.js
@@ -2,21 +2,18 @@ const express = require('express');
 const mongoose = require ('mongoose');
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles');
+const methodOverride = require ('method-override');
 const app = express();
 
-mongoose.connect('mongodb://localhost/blog')
+mongoose.connect('mongodb://localhost/blog');
 
-app.set ('view engine', 'ejs')
-
-app.use(express.urlencoded({extended: false}))
+app.set ('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
     // sorts articles in descending order from time created
-    const articles = await Article.find().sort({
-        createdAt: 'desc'})
-    
-    
-
+    const articles = await Article.find().sort({createdAt: 'desc'})
     res.render(`articles/index`, { articles: articles})
 })
 
